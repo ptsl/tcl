9c4b6385af5955f55f06336f72a5b3839ab1eece
Peter Slizik <Peter.Slizik@iblsoft.com>
2024-05-06 16:45:47 +0200

    Add table::column to table.tcl

--- src/table.tcl
+++ src/table.tcl
@@ -32,4 +32,44 @@ namespace eval table {
     proc row {table index} {
         return [lindex $table $index]
     }
+
+    #
+    # Return the index-th column as a list.
+    #
+    # Usage:
+    #
+    #   set table {
+    #       {1 11 111}
+    #       {2 22 222}
+    #       {3 33 333}
+    #   }
+    #
+    #   table column $table 0 -> {1 2 3}
+    #   table column $table 1 -> {11 22 33}
+    #   table column $table 2 -> {111 222 333}
+    #
+    #   # out of range
+    #   table column $table 3 -> {{} {} {}}    ;# Different from table row.
+    #
+    #   # special indices
+    #   table column $table end -> {111 222 333}
+    #   table column $table 5-4 -> {11 22 33}
+    #
+    #   # special cases
+    #   table column {} 0 -> {}
+    #
+    # Missing values are returned as empty strings.
+    #
+    #   set table {
+    #       {1}
+    #       {2 22}
+    #       {3 33 333}
+    #   }
+    #
+    #   table column $table 1 -> {{} 22 33}
+    #   table column $table 2 -> {{} {} 333}
+    #
+    proc column {table index} {
+        return [lmap row $table {lindex $row $index}]
+    }
 }
