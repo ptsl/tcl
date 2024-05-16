1da4f4fd513edd51e6971802fae4f59e9c09391d
Peter Slizik <Peter.Slizik@iblsoft.com>
2024-05-16 19:24:39 +0200

    Enclose procs in strlib.tcl into a namespace

--- src/strlib.tcl
+++ src/strlib.tcl
@@ -1,66 +1,71 @@
 #!/usr/bin/tclsh
 
-#
-# Return 1 if string ends with the suffix, otherwise return 0.
-#
-# Use the -nocase option for case-insensitive matching.
-#
-# Usage:
-#
-#   endswith efgh abcdefgh
-#   endswith eFgH AbCdEfGh -nocase
-#
-proc endswith {suffix string args} {
-    if {"-nocase" in $args} {
-        return [string match -nocase *$suffix $string]
-    } else {
-        return [string match *$suffix $string]
-    }
-}
+namespace eval strlib {
+    namespace export endswith splitlines startswith
+    namespace ensemble create
 
-#
-# Split text into lines.
-#
-# If the text ends with '\n', the last element is removed from the result to ensure
-# that the function does not produce an empty string after the last line.
-#
-# Examples:
-#
-#   splitlines aaa\nbbb\nccc\n -> {aaa bbb ccc}
-#   splitlines aaa\nbbb\nccc   -> {aaa bbb ccc}
-#   splitlines aaa\nbbb\n      -> {aaa bbb}
-#   splitlines aaa\nbbb        -> {aaa bbb}
-#   splitlines aaa\n           -> {aaa}
-#   splitlines aaa             -> {aaa}
-#   splitlines \n\n\n          -> {{} {} {}}
-#   splitlines \n\n            -> {{} {}}
-#   splitlines \n              -> {{}}
-#   splitlines {}              -> {}
-#
-proc splitlines {text} {
-    set lines [split $text \n]
-
-    if {[string index $text end] eq "\n"} {
-        set lines [lrange $lines 0 end-1]
+    #
+    # Return 1 if string ends with the suffix, otherwise return 0.
+    #
+    # Use the -nocase option for case-insensitive matching.
+    #
+    # Usage:
+    #
+    #   endswith efgh abcdefgh
+    #   endswith eFgH AbCdEfGh -nocase
+    #
+    proc endswith {suffix string args} {
+        if {"-nocase" in $args} {
+            return [string match -nocase *$suffix $string]
+        } else {
+            return [string match *$suffix $string]
+        }
     }
 
-    return $lines
-}
+    #
+    # Split text into lines.
+    #
+    # If the text ends with '\n', the last element is removed from the result to ensure
+    # that the function does not produce an empty string after the last line.
+    #
+    # Examples:
+    #
+    #   splitlines aaa\nbbb\nccc\n -> {aaa bbb ccc}
+    #   splitlines aaa\nbbb\nccc   -> {aaa bbb ccc}
+    #   splitlines aaa\nbbb\n      -> {aaa bbb}
+    #   splitlines aaa\nbbb        -> {aaa bbb}
+    #   splitlines aaa\n           -> {aaa}
+    #   splitlines aaa             -> {aaa}
+    #   splitlines \n\n\n          -> {{} {} {}}
+    #   splitlines \n\n            -> {{} {}}
+    #   splitlines \n              -> {{}}
+    #   splitlines {}              -> {}
+    #
+    proc splitlines {text} {
+        set lines [split $text \n]
 
-#
-# Return 1 if string starts with the prefix, otherwise return 0.
-#
-# Use the -nocase option for case-insensitive matching.
-#
-# Usage:
-#
-#   startswith abcd abcdefgh
-#   startswith aBcD AbCdEfGh -nocase
-#
-proc startswith {prefix string args} {
-    if {"-nocase" in $args} {
-        return [string match -nocase $prefix* $string]
-    } else {
-        return [string match $prefix* $string]
+        if {[string index $text end] eq "\n"} {
+            set lines [lrange $lines 0 end-1]
+        }
+
+        return $lines
+    }
+
+    #
+    # Return 1 if string starts with the prefix, otherwise return 0.
+    #
+    # Use the -nocase option for case-insensitive matching.
+    #
+    # Usage:
+    #
+    #   startswith abcd abcdefgh
+    #   startswith aBcD AbCdEfGh -nocase
+    #
+    proc startswith {prefix string args} {
+        if {"-nocase" in $args} {
+            return [string match -nocase $prefix* $string]
+        } else {
+            return [string match $prefix* $string]
+        }
     }
 }
